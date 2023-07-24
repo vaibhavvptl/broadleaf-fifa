b3db2e420ef5790f070ec940e8449135151a3ce0
Neha-Bhatt-Techholding <neha.bhatt@techholding.co>
2023-07-24 17:35:35 +0530

    feat:added errorhandling for all apis

--- controllers/getProductController.js
+++ controllers/getProductController.js
@@ -1,7 +1,9 @@
 const axios = require("axios");
 const API_URL = process.env.BASE_API_URL;
+const { NotFoundError,InternalServerError,UnauthorizedRequestError,ForbiddenRequestError }=require('../customErrors')
+const errorMessage=require('../errorMessage')
 
-const getProductController = async (req, res) => {
+const getProductController = async (req, res,next) => {
   try {
     // check if access token exists in session
     if (!req.session.accessToken) {
@@ -12,14 +14,22 @@ const getProductController = async (req, res) => {
     // make a request to the events API endpoint with the access token in the Authorization header
     const response = await axios.get(`${API_URL}/api/catalog/products`, {
       headers: {
-        Authorization: `Bearer ${req.session.accessToken}`,
+        // Authorization: `Bearer ${req.session.accessToken}`,
         "x-context-request": `{"tenantId":"${process.env.TENANT_ID}","applicationId":"${process.env.APPLICATION_ID}"}`,
       },
     });
     res.status(200).json(response.data);
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+    if (err.response && err.response.status === 404) {
+      next(new NotFoundError(errorMessage.error404))
+    } else if (err.response && err.response.status === 401) {
+      next(new UnauthorizedRequestError(errorMessage.statusCode401))
+    } else if (err.response && err.response.status === 403) {
+      next(new ForbiddenRequestError(errorMessage.statusCode401))
+    } else {
+      // For all other errors
+      next( new InternalServerError(errorMessage.error500));
+    }
   }
 };
 
